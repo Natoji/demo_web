fc505c3256ffcce5894ec4ececa1af85b5cba9bd
hau <hau@example.com>
2025-07-20 22:02:36 +0700

    Fix: Python version & pywin32 issue for Render

--- be/config/settings.py
+++ be/config/settings.py
@@ -78,11 +78,11 @@ DATABASES = {
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='3306'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
         'OPTIONS': {
-            'charset': 'utf8',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES', NAMES 'utf8'",
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES', NAMES 'utf8mb4'",
         }
     }
 }
