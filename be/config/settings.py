0a3614f43d36b7c52f04171355e8679d11cfe773
hau <hau@example.com>
2025-07-20 23:28:20 +0700

    update

--- be/config/settings.py
+++ be/config/settings.py
@@ -47,7 +47,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'https://trilux.netlify.app',
+]
 
 ROOT_URLCONF = 'config.urls'
 
